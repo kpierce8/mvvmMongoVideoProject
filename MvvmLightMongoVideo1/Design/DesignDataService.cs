8da2b4db5e052d8e5c015e9ae9bc2153b3c3fc7e
Ken Pierce <kpierce8@gmail.com>
2015-12-15 21:24:20 -0800

    initial project commit

---
+++ MvvmLightMongoVideo1/Design/DesignDataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MvvmLightMongoVideo1.Model;
+
+namespace MvvmLightMongoVideo1.Design
+{
+    public class DesignDataService : IDataService
+    {
+        public void GetData(Action<DataItem, Exception> callback)
+        {
+            // Use this to create design time data
+
+            var item = new DataItem("Welcome to MVVM Light [design]");
+            callback(item, null);
+        }
+    }
+}
---
+++ MvvmLightMongoVideo1/ViewModel/MainViewModel.cs
@@ -0,0 +1,82 @@
+﻿using GalaSoft.MvvmLight;
+using MvvmLightMongoVideo1.Model;
+using System;
+using System.Linq;
+
+namespace MvvmLightMongoVideo1.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// See http://www.mvvmlight.net
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+        private readonly IDataService _dataService;
+
+        /// <summary>
+        /// The <see cref="WelcomeTitle" /> property's name.
+        /// </summary>
+        public const string WelcomeTitlePropertyName = "WelcomeTitle";
+
+        private string _welcomeTitle = string.Empty;
+
+        /// <summary>
+        /// Gets the WelcomeTitle property.
+        /// Changes to that property's value raise the PropertyChanged event. 
+        /// </summary>
+        public string WelcomeTitle
+        {
+            get
+            {
+                return _welcomeTitle;
+            }
+            set
+            {
+                Set(ref _welcomeTitle, value);
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel(IDataService dataService)
+        {
+            ListLands();
+
+            _dataService = dataService;
+            _dataService.GetData(
+                (item, error) =>
+                {
+                    if (error != null)
+                    {
+                        // Report error here
+                        return;
+                    }
+
+                    WelcomeTitle = item.Title;
+                });
+        }
+
+
+        private static void ListLands()
+        {
+            using (var context = new ltdilEntities())
+            {
+                var lands = context.theLands.ToList();
+                foreach (var land in lands)
+                {
+                    Console.WriteLine(land.lineValue);
+                }
+            }
+        }
+
+        ////public override void Cleanup()
+        ////{
+        ////    // Clean up if needed
+
+        ////    base.Cleanup();
+        ////}
+    }
+}
